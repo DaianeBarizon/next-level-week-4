052ed0c341c1042e2c13c7c5f8fff014f034a980
Daiane Barizon <daianebarizon22@gmail.com>
2021-03-21 18:22:53 -0300

    feat: create profile and completed challenges components

---
+++ moveit-next/src/components/Profile.tsx
@@ -0,0 +1,13 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src='https://miro.medium.com/fit/c/262/262/0*LGXcOhYLdFkoueMl' alt=' Daiane Barizon' />
+      <div>
+        <strong>Daiane Barizon</strong>
+        <p><img src='icons/level.svg' alt='level' />Level 1</p>
+      </div>
+    </div>
+  )
+}
