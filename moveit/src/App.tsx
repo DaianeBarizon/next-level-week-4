6d0f644a02b7b4afbc222d3c36744e010dc4744a
Daiane Barizon <daianebarizon22@gmail.com>
2021-03-21 16:22:20 -0300

    feat: create experience bar components

--- moveit/src/App.tsx
+++ moveit/src/App.tsx
@@ -1,6 +1,11 @@
+import { ExperienceBar } from './components/ExperienceBar';
+import './styles/global.css';
+
 function App() {
   return (
-    <h1>Hello App</h1>
+    <div className='container'>
+      <ExperienceBar />
+    </div>
   );
 }
 
